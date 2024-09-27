c3581b338f035435d697071a98a0a36950de7aa4
isaac-goldberg <isaacg0ldb3rg@gmail.com>
2024-09-27 01:17:41 -0700

    feat: events page is now scalable

--- src/routes/events.jsx
+++ src/routes/events.jsx
@@ -7,11 +7,29 @@ import "../css/events.css";
 
 const events = [
     {
-        title: "Club Rush",
-        date: "September 13",
-        description: "hello world"
+        title: <><i className="fa-solid fa-handshake"></i> Intro Meeting</>,
+        date: "Date TBD",
+        location: "Room 4221",
+        description: <>Get information on how Comp Sci Club will work this year, when meetings are, who's on the board, and get your first points of the year for the <a href="/leaderboard">leaderboard</a> just by showing up to the first meeting!</>,
+    },
+    {
+        title: <><i className="fa-brands fa-python"></i> Python Basics Meeting</>,
+        date: "Date TBD",
+        location: "Room 4221",
+        description: <>Learn how to get started coding in Python in our first non-introductory meeting! No beginner experience required. You may have the chance to win points and make it to the top of our <a href="/leaderboard">leaderboard</a>!</>
     }
 ];
+
+const eventsElems = events.map((event) => {
+    return (
+        <div className="event">
+            <p className="date">{event.date}</p>
+            <h3>{event.title}</h3>
+            <p className="room">{event.location}</p>
+            <p>{event.description}</p>
+        </div>
+    );
+});
 
 export default function Events() {
     return (
@@ -27,18 +45,7 @@ export default function Events() {
 
                 <div className="events">
                     <Carousel autoPlay={true} infiniteLoop={true} interval={15000} showStatus={false} emulateTouch={true}>
-                        <div className="event">
-                            <p className="date">Date TBD</p>
-                            <h3><i className="fa-solid fa-handshake"></i> Intro Meeting</h3>
-                            <p className="room">Room 4208</p>
-                            <p>Get information on how Comp Sci Club will work this year, when meetings are, who's on the board, and get your first points of the year for the <a href="/leaderboard">leaderboard</a> just by showing up to the first meeting!</p>
-                        </div>
-                        <div className="event">
-                            <p className="date">Date TBD</p>
-                            <h3><i className="fa-brands fa-python"></i> Python Basics Meeting</h3>
-                            <p className="room">Room 4208</p>
-                            <p>Learn how to get started coding in Python in our first non-introductory meeting! No beginner experience required. You may have the chance to win points and make it to the top of our <a href="/leaderboard">leaderboard</a>!</p>
-                        </div>
+                        {eventsElems}
                     </Carousel>
                 </div>
             </div>
