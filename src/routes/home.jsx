4fe6f3ef60668500fb9350bb01adfe0751316f7a
neelkarkhanis <neelkarkha71@gmail.com>
2024-10-06 00:20:16 -0700

    add image (3rd try)

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
             
             <div className="page home">
                 <h1>West High Computer Science Club</h1>
-                <img src="file:///Users/neel/Desktop/Screenshot%202024-10-05%20at%2011.58.12%E2%80%AFPM.png"></img>
+                <img src="file:///Users/neel/Desktop/Screenshot%202024-10-05%20at%2011.58.12%E2%80%AFPM.png"/>
                 <p>Learn how to code, compete in Hackathons, and hang out with other students interested in CS at West!</p>
                 <p>Check out the <a href="/events">upcoming events</a> page for meeting dates. Once the club gets underway, you'll have the opportunity to climb the <a href="/leaderboard">leaderboard</a> by attending meetings and winning competitions!</p>
             </div>
